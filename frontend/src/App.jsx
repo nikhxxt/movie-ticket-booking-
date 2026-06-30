799485cd40f00dbabe9e4ab27c7cd5ccf605f790
nikhxxt <nikhxtwork@gmail.com>
2026-06-30 17:17:28 +0530

    Create App component with multiple subcomponents

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import Navbar from "./components/Navbar";
+import AddMovie from "./components/AddMovie";
+import GenreFilter from "./components/GenreFilter";
+import MovieList from "./components/MovieList";
+import AddShowtime from "./components/AddShowtime";
+import SeatGrid from "./components/SeatGrid";
+import BookingForm from "./components/BookingForm";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <AddMovie />
+
+        <GenreFilter />
+
+        <MovieList />
+
+        <AddShowtime />
+
+        <SeatGrid />
+
+        <BookingForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
