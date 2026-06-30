58969e9945a1318c247c18968127333a5695de66
nikhxxt <nikhxtwork@gmail.com>
2026-06-30 16:11:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,39 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
+
+// Import Routes
+const movieRoutes = require("./routes/movieRoutes");
+const showtimeRoutes = require("./routes/showtimeRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
 
+// Connect Database
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Movie Booking API Running...");
+  res.send("🎬 Movie Ticket Booking API is Running...");
+});
+
+// API Routes
+app.use("/api/movies", movieRoutes);
+app.use("/api/showtimes", showtimeRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+// Invalid Route
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
