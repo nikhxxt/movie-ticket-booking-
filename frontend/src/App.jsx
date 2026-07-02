3af994e37c260c3687133355ae1eb981afc95a6a
nikhxxt <nikhxtwork@gmail.com>
2026-07-02 18:11:57 +0530

    Update App to include routing for login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import AddMovie from "./components/AddMovie";
 import GenreFilter from "./components/GenreFilter";
@@ -5,22 +6,50 @@ import MovieList from "./components/MovieList";
 import AddShowtime from "./components/AddShowtime";
 import SeatGrid from "./components/SeatGrid";
 import BookingForm from "./components/BookingForm";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  return (
-    <>
-      <Navbar />
+  const [currentPage, setCurrentPage] = useState("home");
 
-      <div className="container">
-        <AddMovie />
-        <GenreFilter />
-        <MovieList />
-        <AddShowtime />
-        <SeatGrid />
-        <BookingForm />
-      </div>
-    </>
-  );
+  // Simple routing based on hash or current page state
+  useEffect(() => {
+    const handleHashChange = () => {
+      const hash = window.location.hash.slice(1) || "home";
+      setCurrentPage(hash);
+    };
+
+    window.addEventListener("hashchange", handleHashChange);
+    handleHashChange();
+
+    return () => window.removeEventListener("hashchange", handleHashChange);
+  }, []);
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "login":
+        return <Login />;
+      case "signup":
+        return <Signup />;
+      case "home":
+      default:
+        return (
+          <>
+            <Navbar />
+            <div className="container">
+              <AddMovie />
+              <GenreFilter />
+              <MovieList />
+              <AddShowtime />
+              <SeatGrid />
+              <BookingForm />
+            </div>
+          </>
+        );
+    }
+  };
+
+  return <>{renderPage()}</>;
 }
 
 export default App;
